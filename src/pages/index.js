2b5ef8e283fb3e30465c747fccd084e4eb7c54cb
Khaled Garbaya <kgarbaya@gmail.com>
2021-01-01 17:20:07 +0100

    04-finish

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,26 @@
 import React from 'react'
+import {graphql, Link} from 'gatsby'
+const IndexPage = ({data}) => {
+  return (
+    <ul>
+      {data.allShopifyProduct.nodes.map((product) => (
+        <li key={product.shopifyId}>
+          <Link to={product.handle}>{product.title}</Link>
+        </li>
+      ))}
+    </ul>
+  )
+}
 
-const IndexPage = () => <h1>Hello World</h1>
-
+export const query = graphql`
+  query productsQuery {
+    allShopifyProduct {
+      nodes {
+        title
+        shopifyId
+        handle
+      }
+    }
+  }
+`
 export default IndexPage
---
+++ src/pages/{ShopifyProduct.handle}.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import {graphql} from 'gatsby'
+
+const ProductPage = ({data}) => {
+  return (
+    <div>
+      <h2>{data.shopifyProduct.title}</h2>
+      <div>
+        <img
+          width={300}
+          src={data.shopifyProduct.images[0].originalSrc}
+          alt={data.shopifyProduct.title}
+        />
+      </div>
+      <div
+        dangerouslySetInnerHTML={{__html: data.shopifyProduct.descriptionHtml}}
+      />
+      <span>
+        {data.shopifyProduct.priceRange.maxVariantPrice.currencyCode}{' '}
+        {data.shopifyProduct.priceRange.maxVariantPrice.amount}
+      </span>
+    </div>
+  )
+}
+export const query = graphql`
+  query($handle: String) {
+    shopifyProduct(handle: {eq: $handle}) {
+      title
+      shopifyId
+      handle
+      descriptionHtml
+      priceRange {
+        maxVariantPrice {
+          amount
+          currencyCode
+        }
+        minVariantPrice {
+          amount
+          currencyCode
+        }
+      }
+      images {
+        originalSrc
+      }
+    }
+  }
+`
+export default ProductPage
